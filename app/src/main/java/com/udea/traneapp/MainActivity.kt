e1090241b780711a71564410d2fc5c2c62d5fcc7
Santiago397 <santilopez397@gmail.com>
2021-12-19 18:44:02 -0500

    recycler view done

--- app/src/main/java/com/udea/traneapp/MainActivity.kt
+++ app/src/main/java/com/udea/traneapp/MainActivity.kt
@@ -1,30 +1,36 @@
 package com.udea.traneapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.udea.traneapp.Utils.Utils
-import org.json.JSONArray
+import com.udea.traneapp.model.City
 import org.json.JSONException
 import org.json.JSONObject
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), RecyclerAdapter.OnCityClickListener {
 
-    var countryNames: ArrayList<String> = ArrayList()
-    var cityNames: ArrayList<String> = ArrayList()
-    var cityContact: ArrayList<String> = ArrayList()
-    var cityDescrip: ArrayList<String> = ArrayList()
-    var cityUrlImg: ArrayList<String> = ArrayList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupRecyclerView()
+    }
 
+    private fun setupRecyclerView() {
         val recyclerView: RecyclerView = findViewById(R.id.recyclerView)
-        val linearLayoutManager = LinearLayoutManager(applicationContext)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        //Get the info from the json and put it in a List
+        val cityList = getInfo()
 
-        recyclerView.layoutManager = linearLayoutManager
+        recyclerView.adapter = RecyclerAdapter(this, cityList, this)
+    }
+
+    private fun getInfo(): List<City> {
+
+        val cityList = mutableListOf<City>()
 
         try {
             val obj = JSONObject(Utils(this).getJsonObject(applicationContext))
@@ -33,27 +39,42 @@ class MainActivity : AppCompatActivity() {
             for (i in 0 until citiesArray.length()) {
                 val cityData = citiesArray.getJSONObject(i)
 
-                countryNames.add(cityData.getString("country"))
-                cityNames.add(cityData.getString("pCity"))
-                //usar JSONObject a ver si cambia los valores de los slashes
-                cityUrlImg.add(cityData.getJSONArray("urlImg").toString())
+                var cityName = cityData.getString("pCity")
+                var countryName = cityData.getString("country")
+                var cityDescription = cityData.getString("description")
+                var cityRate = cityData.getString("rate")
+                var cityImages: ArrayList<String> = ArrayList()
+
+                var imgArray = cityData.getJSONArray("urlImg")
+
+                for (a in 0 until imgArray.length()) {
+                    cityImages.add(imgArray.get(a).toString())
+                }
+
+                cityList.add(City(cityName, countryName, cityDescription, cityRate, cityImages))
+
             }
 
         } catch (ex: JSONException) {
-            return ex.printStackTrace()
+            ex.printStackTrace()
         }
 
-        val cityAdapter = CityAdapter(
-            this,
-            countryNames,
-            cityNames,
-            cityUrlImg
-        )
-
-        recyclerView.adapter = cityAdapter
-
-
+        return cityList
     }
 
-
+    override fun onButtonClick(
+        cityName: String,
+        countryName: String,
+        cityDescription: String,
+        cityImages: ArrayList<String>,
+        cityRate: String
+    ) {
+        val intent = Intent(this, CityDetail::class.java)
+        intent.putExtra("cityName", cityName)
+        intent.putExtra("countryName", countryName)
+        intent.putExtra("cityDescription", cityDescription)
+        intent.putStringArrayListExtra("cityImages", cityImages)
+        intent.putExtra("cityRate", cityRate)
+        startActivity(intent)
+    }
 }
