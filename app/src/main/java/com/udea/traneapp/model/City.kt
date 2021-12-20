6334f4f8a6bc0f8f6fa4b95ca5b12f6386a2895b
Santiago397 <santilopez397@gmail.com>
2021-12-20 10:42:16 -0500

    Recyclerview is works with the details of each item

---
+++ app/src/main/java/com/udea/traneapp/model/City.kt
@@ -0,0 +1,9 @@
+package com.udea.traneapp.model
+
+data class  City(
+    val cityName:String,
+    val countryName:String,
+    val cityDescription:String,
+    val cityRate:String,
+    val cityImages: ArrayList<String>
+    )
